4465d423847c9838a859e24ebdb9a1ab62726b64
Christopher Lock <joutcast@gmail.com>
2018-06-03 09:08:57 -0700

    Update Android.mk

--- Android.mk
+++ Android.mk
@@ -15,5 +15,10 @@
 # limitations under the License.
 #
 
-LOCAL_PATH := $(call my-dir)
+ifneq ($(filter tc4_k4008,$(TARGET_DEVICE)),)
+
+LOCAL_PATH := device/ojji/tc4_k4008/
+
 include $(call all-makefiles-under,$(LOCAL_PATH))
+
+endif
