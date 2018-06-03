d0bd731492e4ffb5666ce35bbaf0c19c43b86929
Christopher Lock <joutcast@gmail.com>
2018-06-03 09:55:39 -0700

    Update cm.mk

--- cm.mk
+++ cm.mk
@@ -1,17 +1,17 @@
 # Release name
-PRODUCT_RELEASE_NAME := Lemon_S8
+PRODUCT_RELEASE_NAME := tc4_k4008_OJJI_4p4
 
 # Inherit device configuration
-$(call inherit-product, $(LOCAL_PATH)/Lemon_S8.mk)
+$(call inherit-product, $(LOCAL_PATH)/tc4_k4008.mk)
 
 # Camera
 PRODUCT_PACKAGES += \
     Snap
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := Lemon_S8
-PRODUCT_NAME := cm_Lemon_S8
-PRODUCT_BRAND := Lemon
-PRODUCT_MODEL := Lemon_S8
-PRODUCT_MANUFACTURER := Lemon
-PRODUCT_CHARACTERISTICS := Lemon_S8
+PRODUCT_DEVICE := tc4_k4008
+PRODUCT_NAME := tc4_k4008_OJJI_4p4
+PRODUCT_BRAND := OJJI
+PRODUCT_MODEL := OJJI
+PRODUCT_MANUFACTURER := OJJI
+PRODUCT_CHARACTERISTICS := OJJI
